bf25177d7da897853d856714a75cd1c5bfb30ede
Carlos De La Roca <carlosdlroca80@gmail.com>
2020-12-13 18:14:56 -0800

    edit blog post template

--- src/templates/template-blog-post.js
+++ src/templates/template-blog-post.js
@@ -6,10 +6,9 @@ import "katex/dist/katex.min.css"
 export default function Template({ data }) {
   const { markdownRemark: post } = data
   return (
-    <div className="blog-post-container h-screen bg-red-900">
-      <Helmet title={`Your Blog Name - ${post.frontmatter.title}`} />
-      <div className="blog-post h-full">
-        <h1 className="text-center text-gray-50">{post.frontmatter.title}</h1>
+    <div className="blog-post-container min-h-screen bg-orange-300 pb-10">
+      <Helmet title={`Math 10 Project - ${post.frontmatter.title}`} />
+      <div className="blog-post h-full py-5">
         <div
           className="blog-post-content w-11/12 lg:max-w-5xl mx-auto bg-white text-coolGray-800 p-4"
           dangerouslySetInnerHTML={{ __html: post.html }}
