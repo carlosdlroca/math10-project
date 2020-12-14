a3a5d7bba46b35ef11b5a304d2ad028025bc21fa
Carlos De La Roca <carlosdlroca80@gmail.com>
2020-12-14 00:05:50 -0800

    add link to index page in template page

--- src/templates/template-blog-post.js
+++ src/templates/template-blog-post.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { Helmet } from "react-helmet"
-import { graphql } from "gatsby"
+import { Link, graphql } from "gatsby"
 import "katex/dist/katex.min.css"
 
 export default function Template({ data }) {
@@ -9,6 +9,12 @@ export default function Template({ data }) {
     <div className="blog-post-container min-h-screen bg-orange-300 pb-10">
       <Helmet title={`Math 10 Project - ${post.frontmatter.title}`} />
       <div className="blog-post h-full py-5">
+        <Link
+          to="/"
+          className="text-4xl text-white bg-blue-400 px-5 py-1 rounded"
+        >
+          Home
+        </Link>
         <div
           className="blog-post-content w-11/12 lg:max-w-5xl mx-auto bg-white text-coolGray-800 p-4"
           dangerouslySetInnerHTML={{ __html: post.html }}
